96aabd120a20fe832d79fc86ced12060e385d800
6470225021 <6470225021@student.chula.ac.th>
2022-04-04 09:48:22 +0700

    Add Login function and Cookie parser.

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,13 +16,44 @@ exports.register = async (req, res, next)=>{
             role
         })
 
-        //Create token
         sendTokenResponse(user, 200, res);
     }catch(err){
         res.status(400).json({success: false});
         console.log(err.stack);
     }
 }
+
+//@desc     Login user
+//@route    POST /api/v1/auth/login
+//@access   Public
+exports.login = async (req, res, next)=>{
+    try{
+        const {email, password} = req.body;
+
+        //Validate email & password
+        if(!email || !password){
+            return res.status(400).json({success: false, msg: 'Please provide an email and password'});
+        }
+    
+        //Check for user
+        const user = await User.findOne({email}).select('+password');
+    
+        if(!user){
+            return res.status(400).json({success: false, msg: 'Invalid credentials'});
+        }
+    
+        //Check if password matches
+        const isMatch = await user.matchPassword(password);
+    
+        if(!isMatch){
+            return res.status(401).json({success: false, msg: 'Invalid credentials'});
+        }
+
+        sendTokenResponse(user, 200, res);
+    }catch(err){
+        return res.status(401).json({success: false, msg: 'Cannot convert email and password to string'});
+    }
+}//close exports.login
 
 //Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res)=>{
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
 //Route files
@@ -16,6 +17,9 @@ const app=express();
 
 //add body parser
 app.use(express.json());
+
+//Cookie parser
+app.use(cookieParser());
 
 //Mount routers
 app.use('/api/v1/dentists',dentists);
